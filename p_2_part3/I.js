48b121bc781c7345781ddad56d0eb1bc5291c91f
Hugooo753 <98612683+hugochao@users.noreply.github.com>
2022-02-24 09:08:13 -0500

    Update I.js

--- p_2_part3/I.js
+++ p_2_part3/I.js
@@ -3,10 +3,25 @@ $(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 50) {
+    if (scroll >= 20) {
       text.removeClass("hidden");
     } else {
       text.addClass("hidden");
     }
+
+  });
+});
+
+$(function() {
+  var text = $(".text2");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 25) {
+      text2.removeClass("hidden");
+    } else {
+      text2.addClass("hidden");
+    }
+
   });
 });
