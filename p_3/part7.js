31c9530eb00fae27c19893a204c76d6ba824e1ef
Hugooo753 <98612683+hugochao@users.noreply.github.com>
2022-04-11 21:31:04 -0400

    Update part7.js

--- p_3/part7.js
+++ p_3/part7.js
@@ -44,3 +44,47 @@ for (var p = 0; p < l; p++) {
 // display the image  
 document.body.appendChild(newImage);
 }
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const channel = 'receipts-w-xyabskm1y';
+
+    fetch(`https://api.are.na/v2/channels/${channel}?per=100`, {cache: 'no-store'})
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        renderTitle(data);
+      });
+    
+  });
+
+  const renderTitle = (data) => {
+    let title = data['title'];
+    document.getElementById('channel-title').innerText = title;
+  };
+
+  const renderBlocks = (data) => {
+    let blocks = data['contents'];
+    blocks.forEach((block) => {
+      console.log(block);
+      let blockclass = block ['class'];
+      if (blockClass == 'Image'){
+        renderImage(block);
+      }
+    });
+  };
+
+const renderImage = (block) => {
+let imageUrl = block['image']['display']['url'];
+let iamgeBlockTemplate = document.getElementById('image-block');
+let clone = iamgeBlockTemplate.content.cloneNode(true);
+
+clone.querySelector('img').src = imageUrl;
+
+document.getElementById('contents').appendChild(clone);
+}
